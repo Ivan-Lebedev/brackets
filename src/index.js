31386f4fd0064ef5653c331ada72aaec08dd30a8
ivan <ivieee@yandex.ru>
2020-09-30 21:07:19 +0400

    task completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  while (str.length > 2) {
+    const constLength = str.length
+    let previousLength
+    for (let i = 0; i < bracketsConfig.length; i++) {
+      str = str.replace(bracketsConfig[i].join(''), '')
+      previousLength = str.length
+    }
+    if (previousLength === constLength) return false
+  }
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    if (str === bracketsConfig[i].join('') || !str) return true
+  }
+  return false
 }
